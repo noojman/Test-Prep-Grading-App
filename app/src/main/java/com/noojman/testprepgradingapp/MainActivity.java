26bb9445fb3f1f9691a8c1eaa5f109d340274d35
Namjoon <eric_joo@ymail.com>
2019-02-15 08:35:52 -0800

    New updated project. Base setup for app.

---
+++ app/src/main/java/com/noojman/testprepgradingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.noojman.testprepgradingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
