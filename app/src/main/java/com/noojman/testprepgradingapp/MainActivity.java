ea77851d28de7cd81aae16a4aabc331e7106ddd2
Namjoon <eric_joo@ymail.com>
2019-02-22 09:34:43 -0800

    Basic test taking layout implemented using a reusable problem layout.

--- app/src/main/java/com/noojman/testprepgradingapp/MainActivity.java
+++ app/src/main/java/com/noojman/testprepgradingapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.noojman.testprepgradingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.noojman.testprepgradingapp.fragments.BubbleSheetFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    BubbleSheetFragment bubbleSheetFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_bubblesheet);
     }
 }
---
+++ app/src/main/java/com/noojman/testprepgradingapp/fragments/BubbleSheetFragment.java
@@ -0,0 +1,19 @@
+package com.noojman.testprepgradingapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.noojman.testprepgradingapp.R;
+
+public class BubbleSheetFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bubblesheet, container, false);
+    }
+}
